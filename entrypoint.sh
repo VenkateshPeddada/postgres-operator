783c1ec6be245a57e782874fc819c8f45a3bff79
Peddada <vepeddada@deloitte.com>
2020-04-17 14:39:16 +0530

    entrypoint with api-server changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#trap revoke_service_account_tokens EXIT
+trap revoke_service_account_tokens EXIT
 
 operator_pod_running() {
     CHECK_OPERATOR_SLEEP=10
@@ -34,107 +34,23 @@ operator_pod_running() {
 
     return 1
 }
+echo Processing ${OPERATOR_COMMAND} request at ${RUN_TIME}
 
-#revoke_service_account_tokens() {
-#    kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get secrets --namespace ${BASE_KUBERNETES_NAMESPACE} | grep ${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT}-token | awk '{print $1}' | xargs -I {} kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete secret {} --namespace ${BASE_KUBERNETES_NAMESPACE}
-#}
-#setup(){
+# Get the Kubernetes API server to use
+if [[ ! -z ${KUBERNETES_INTERNAL_API_SERVER} ]] && [[ "${KUBERNETES_INTERNAL_API_SERVER}" != "" ]];
+then
+    KUBERNETES_API_SERVER="https://${KUBERNETES_INTERNAL_API_SERVER}"
+else
+    KUBERNETES_API_SERVER=`cat /kubeconfig/kubeconfig | grep server: | awk '{print $NF}'`
+fi
 
-#	echo "Ensuring project dependencies..."
-#	EVTDIR="/home/default/operator/bin/pgo-event"
+# Authenticate to Kubernetes using provided kubeconfig and serviceaccount token
+export KUBECONFIG=/kubeconfig/kubeconfig
+alias kubectl_token="kubectl --server=${KUBERNETES_API_SERVER} --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN}"
 
-#	mkdir $GOPATH
-#	chmod 775 $GOPATH
-	
-#	mkdir $GOBIN
-#	chmod 775 $GOBIN
-	
-	# Precondition checks
-#	if [ "$GOPATH" = "" ]; then
-		# Alternatively, take dep approach of go env GOPATH later in the process
-#		echo "GOPATH not defined, exiting..." >&2
-#		exit 1
-#	fi
-#	if ! (echo $PATH | egrep -q "$GOPATH/bin") ; then
-#		echo '$GOPATH/bin not part of $PATH, exiting...' >&2
-#		exit 2
-#	fi
-
-
-	# Idempotent installations
-#	if (yum repolist | egrep -q '^epel/') ; then
-#		echo "Confirmed EPEL repo exists..."
-#	else
-#		echo "=== Installing EPEL ==="
-		# Prefer distro-managed epel-release if it exists (e.g. CentOS)
-#		if (yum -q list epel-release 2>/dev/null); then
-#			sudo yum -y install epel-release
-#		else
-#			sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-#		fi
-#	fi
-
-#	if which go; then
-#		echo -n "  Found: " && go version
-#	else
-#		echo "=== Installing golang ==="
-#		 yum -y install golang
-#	fi
-
-#	if ! [ -f $EVTDIR/nsqd -a -f $EVTDIR/nsqadmin ]; then
-#		echo "=== Installing NSQ binaries ==="
-#		NSQ=nsq-1.1.0.linux-amd64.go1.10.3
-#		curl -o $NSQ.tar.gz https://s3.amazonaws.com/bitly-downloads/nsq/$NSQ.tar.gz
-#		tar xz -f nsq-1.1.0.linux-amd64.go1.10.3.tar.gz -C $EVTDIR
-#		rm -rf nsq-1.1.0.linux-amd64.go1.10.3.tar.gz
-#		cd $EVTDIR
-#		mv $NSQ/bin/* .
-#		rm -rf $NSQ
-		
-#	fi
-
-#	if which docker; then
-		# Suppress errors for this call, as docker returns non-zero when it can't talk to the daemon
-#		set +e
-#		echo -n "  Found: " && docker version --format '{{.Client.Version}}' 2>/dev/null
-#		set -e
-#	else
-#		echo "=== Installing docker ==="
-#		if [ -f /etc/centos-release ]; then
-#			sudo yum -y install docker
-#		else
-#			sudo yum -y install docker --enablerepo=rhel-7-server-extras-rpms
-#		fi
-#	fi
-
-#	if which buildah; then
-#		echo -n "  Found: " && buildah --version
-#	else
-#		echo "=== Installing buildah ==="
-#		if [ -f /etc/centos-release ]; then
-#			sudo yum -y install buildah
-#		else
-#			sudo yum -y install buildah --enablerepo=rhel-7-server-extras-rpms
-#		fi
-#	fi
-
-#	if which dep; then
-#		echo -n "  Found: " && (dep version | egrep '^ version')
-#	else
-#		echo "=== Installing dep ==="
-#		curl -S https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-#	fi
-
-#	if which expenv; then
-#		echo "  Found expenv"
-#	else
-#		echo "=== Installing expenv ==="
-#		yum install -y git
-		# TODO: expenv uses Go modules, could retrieve specific version
-#		go get github.com/blang/expenv
-#	fi
-
-#}
+revoke_service_account_tokens() {
+    kubectl --server=${KUBERNETES_API_SERVER} --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get secrets --namespace ${BASE_KUBERNETES_NAMESPACE} | grep ${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT}-token | awk '{print $1}' | xargs -I {} kubectl --server=${KUBERNETES_API_SERVER} --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete secret {} --namespace ${BASE_KUBERNETES_NAMESPACE}
+}
 
 installrbac(){
 
@@ -144,9 +60,9 @@ installrbac(){
 	cleanup_rbac
 	
 	# see if CRDs need to be created
-	#kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get crd pgclusters.crunchydata.com > /dev/null
+	#kubectl_token get crd pgclusters.crunchydata.com > /dev/null
 	#if [ $? -eq 1 ]; then
-	#	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} create -f $DIR/crd.yaml
+	#	kubectl_token create -f $DIR/crd.yaml
 	#fi
 
 	# create the initial pgo admin credential
@@ -154,29 +70,29 @@ installrbac(){
 	install_bootstrap_creds
 
 	# create the cluster roles one time for the entire Kube cluster
-	expenv -f $DIR/configmap-yaml/cluster-roles.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} create -f -
+	expenv -f $DIR/configmap-yaml/cluster-roles.yaml | kubectl_token create -f -
 
 
 	# create the Operator service accounts
-	expenv -f $DIR/configmap-yaml/service-accounts.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/service-accounts.yaml | kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create -f -
 
 	if [ -r "$PGO_IMAGE_PULL_SECRET_MANIFEST" ]; then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE create -f "$PGO_IMAGE_PULL_SECRET_MANIFEST"
+		kubectl_token -n $PGO_OPERATOR_NAMESPACE create -f "$PGO_IMAGE_PULL_SECRET_MANIFEST"
 	fi
 
 	if [ -n "$PGO_IMAGE_PULL_SECRET" ]; then
 		patch='{"imagePullSecrets": [{ "name": "'"$PGO_IMAGE_PULL_SECRET"'" }]}'
 
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE patch --type=strategic --patch="$patch" serviceaccount/postgres-operator
+		kubectl_token -n $PGO_OPERATOR_NAMESPACE patch --type=strategic --patch="$patch" serviceaccount/postgres-operator
 	fi
 
 	# create the cluster role bindings to the Operator service accounts
 	# postgres-operator and pgo-backrest, here we are assuming a single
 	# Operator in the PGO_OPERATOR_NAMESPACE env variable
-	expenv -f $DIR/configmap-yaml/cluster-role-bindings.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/cluster-role-bindings.yaml | kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create -f -
 
-	expenv -f $DIR/configmap-yaml/roles.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE create -f -
-	expenv -f $DIR/configmap-yaml/role-bindings.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/roles.yaml | kubectl_token -n $PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/role-bindings.yaml | kubectl_token -n $PGO_OPERATOR_NAMESPACE create -f -
 
 	# create the keys used for pgo API
 	source $DIR/gen-api-keys.sh
@@ -188,34 +104,34 @@ installrbac(){
 
 cleanup_rbac(){
 	
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE get serviceaccount postgres-operator  > /dev/null 2> /dev/null
+	kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE get serviceaccount postgres-operator  > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE delete serviceaccount postgres-operator
+		kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE delete serviceaccount postgres-operator
 	fi
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get clusterrole pgo-cluster-role   > /dev/null 2> /dev/null
+	kubectl_token get clusterrole pgo-cluster-role   > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete clusterrole pgo-cluster-role 
+		kubectl_token delete clusterrole pgo-cluster-role 
 	fi
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get clusterrolebinding pgo-cluster-role   > /dev/null 2> /dev/null
+	kubectl_token get clusterrolebinding pgo-cluster-role   > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete clusterrolebinding pgo-cluster-role  > /dev/null 2> /dev/null
+		kubectl_token delete clusterrolebinding pgo-cluster-role  > /dev/null 2> /dev/null
 	fi
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE get role pgo-role   > /dev/null 2> /dev/null
+	kubectl_token -n $PGO_OPERATOR_NAMESPACE get role pgo-role   > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE delete role pgo-role 
+		kubectl_token -n $PGO_OPERATOR_NAMESPACE delete role pgo-role 
 	fi
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE get rolebinding pgo-role   > /dev/null 2> /dev/null
+	kubectl_token -n $PGO_OPERATOR_NAMESPACE get rolebinding pgo-role   > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} -n $PGO_OPERATOR_NAMESPACE delete rolebinding pgo-role  > /dev/null
+		kubectl_token -n $PGO_OPERATOR_NAMESPACE delete rolebinding pgo-role  > /dev/null
 	fi
 
 	sleep 5
@@ -228,19 +144,19 @@ install_bootstrap_creds(){
 
 
 	# see if the bootstrap pgorole Secret exists or not, deleting it if found
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get secret pgorole-$PGOADMIN_ROLENAME -n $PGO_OPERATOR_NAMESPACE 2> /dev/null > /dev/null
+	kubectl_token get secret pgorole-$PGOADMIN_ROLENAME -n $PGO_OPERATOR_NAMESPACE 2> /dev/null > /dev/null
 	if [ $? -eq 0 ]; then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete secret pgorole-$PGOADMIN_ROLENAME -n $PGO_OPERATOR_NAMESPACE
+		kubectl_token delete secret pgorole-$PGOADMIN_ROLENAME -n $PGO_OPERATOR_NAMESPACE
 	fi
 
-	expenv -f $DIR/configmap-yaml/pgorole-pgoadmin.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} create -f -
+	expenv -f $DIR/configmap-yaml/pgorole-pgoadmin.yaml | kubectl_token create -f -
 
 	# see if the bootstrap pgouser Secret exists or not, deleting it if found
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get secret pgouser-$PGOADMIN_USERNAME -n $PGO_OPERATOR_NAMESPACE  2> /dev/null > /dev/null
+	kubectl_token get secret pgouser-$PGOADMIN_USERNAME -n $PGO_OPERATOR_NAMESPACE  2> /dev/null > /dev/null
 	if [ $? -eq 0 ]; then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} delete secret pgouser-$PGOADMIN_USERNAME -n $PGO_OPERATOR_NAMESPACE
+		kubectl_token delete secret pgouser-$PGOADMIN_USERNAME -n $PGO_OPERATOR_NAMESPACE
 	fi
-	expenv -f $DIR/configmap-yaml/pgouser-admin.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} create -f -
+	expenv -f $DIR/configmap-yaml/pgouser-admin.yaml | kubectl_token create -f -
 }
 
 
@@ -259,12 +175,12 @@ setupnamespaces(){
 	fi
 
 	echo "creating "$PGO_OPERATOR_NAMESPACE" namespace to deploy the Operator into..."
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get namespace $PGO_OPERATOR_NAMESPACE > /dev/null 2> /dev/null
+	kubectl_token get namespace $PGO_OPERATOR_NAMESPACE > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
 		echo namespace $PGO_OPERATOR_NAMESPACE is already created
 	else
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} create namespace $PGO_OPERATOR_NAMESPACE > /dev/null
+		kubectl_token create namespace $PGO_OPERATOR_NAMESPACE > /dev/null
 		echo namespace $PGO_OPERATOR_NAMESPACE created
 	fi
 
@@ -274,7 +190,7 @@ setupnamespaces(){
 	echo "creating namespaces for the Operator to watch and create PG clusters into..."
 	for ns in "${array[@]}"
 	do
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get namespace $ns > /dev/null 2> /dev/null
+		kubectl_token get namespace $ns > /dev/null 2> /dev/null
 
 		if [ $? -eq 0 ]
 		then
@@ -295,7 +211,7 @@ deployoperator(){
 
 	$DIR/cleanup.sh
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get clusterrole pgo-cluster-role 2> /dev/null > /dev/null
+	kubectl_token get clusterrole pgo-cluster-role 2> /dev/null > /dev/null
 	if [ $? -ne 0 ]
 	then
 		echo ERROR: pgo-cluster-role was not found 
@@ -306,7 +222,7 @@ deployoperator(){
 	#
 	# credentials for pgbackrest sshd 
 	#
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create secret generic pgo-backrest-repo-config \
+	kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create secret generic pgo-backrest-repo-config \
 		--from-file=config=$PGOROOT/conf/pgo-backrest-repo/config \
 		--from-file=sshd_config=$PGOROOT/conf/pgo-backrest-repo/sshd_config \
 		--from-file=aws-s3-credentials.yaml=$PGOROOT/conf/pgo-backrest-repo/aws-s3-credentials.yaml \
@@ -315,15 +231,15 @@ deployoperator(){
 	#
 	# credentials for pgo-apiserver TLS REST API
 	#
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE get secret pgo.tls > /dev/null 2> /dev/null
+	kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE get secret pgo.tls > /dev/null 2> /dev/null
 	if [ $? -eq 0 ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE delete secret pgo.tls
+		kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE delete secret pgo.tls
 	fi
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create secret tls pgo.tls --key=$PGOROOT/conf/postgres-operator/server.key --cert=$PGOROOT/conf/postgres-operator/server.crt
+	kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create secret tls pgo.tls --key=$PGOROOT/conf/postgres-operator/server.key --cert=$PGOROOT/conf/postgres-operator/server.crt
 
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create configmap pgo-config \
+	kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create configmap pgo-config \
 		--from-file=$PGOROOT/conf/postgres-operator \
 		--from-file=pgo.yaml=$PGOROOT/conf/postgres-operator/configmap-yaml/pgo.yaml
 
@@ -341,24 +257,18 @@ deployoperator(){
 	#
 	# create the postgres-operator Deployment and Service
 	#
-	expenv -f $DIR/configmap-yaml/deployment.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create -f -
-	expenv -f $DIR/configmap-yaml/service.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/deployment.yaml | kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/service.yaml | kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create -f -
 
-	expenv -f $DIR/configmap-yaml/pgo-client.yaml | kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} --namespace=$PGO_OPERATOR_NAMESPACE create -f -
+	expenv -f $DIR/configmap-yaml/pgo-client.yaml | kubectl_token --namespace=$PGO_OPERATOR_NAMESPACE create -f -
 }
-
-echo Processing ${OPERATOR_COMMAND} request at ${RUN_TIME}
-
-# Authenticate to Kubernetes using provided kubeconfig and serviceaccount token
-export KUBECONFIG=/kubeconfig/kubeconfig
-alias kubectl_token="kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN}"
 
 export GOPATH=/odev
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 export NAMESPACE=${KUBERNETES_NAMESPACE}
 export PGO_OPERATOR_NAMESPACE=${KUBERNETES_NAMESPACE}
-export PGO_CMD="kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN}"
+export PGO_CMD="kubectl_token"
 export PGOROOT=/home/default/operator
 export PGO_IMAGE_TAG=$PGO_BASEOS-$PGO_VERSION
 export PGO_CA_CERT=$PGOROOT/conf/postgres-operator/server.crt
@@ -407,8 +317,8 @@ case "${OPERATOR_COMMAND}" in
 	# Apply Cluster
     echo RUN PGO command in ${KUBERNETES_NAMESPACE} namespace
     #kubectl_token apply -f /home/default/operator/cr.yaml
-	pod_name=`kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo create cluster ${CLUSTER_NAME}
+	pod_name=`kubectl_token get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
+	kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo create cluster ${CLUSTER_NAME}
 	
     sleep 10
 ;;
@@ -421,15 +331,15 @@ case "${OPERATOR_COMMAND}" in
 
     # Apply backup
     echo Applying backup.yaml in ${KUBERNETES_NAMESPACE} namespace
-    pod_name=`kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
+    pod_name=`kubectl_token get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
 	if [ "${BACKUP_TYPE}" == "FULL" ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=full"
+		kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=full"
 	elif [ "${BACKUP_TYPE}" == "DIFF" ]
 	then
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=diff"
+		kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=diff"
 	else
-		kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=incr"	
+		kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo backup  ${CLUSTER_NAME} --backup-opts="--type=incr"	
 	fi
 	
     sleep 10
@@ -445,7 +355,7 @@ case "${OPERATOR_COMMAND}" in
     # Apply restore
     echo Applying restore.yaml in ${KUBERNETES_NAMESPACE} namespace
 	
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo restore ${CLUSTER_NAME} --backup-opts="--type=full" --no-prompt 
+	kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo restore ${CLUSTER_NAME} --backup-opts="--type=full" --no-prompt 
 	
     sleep 10
 ;;
@@ -458,8 +368,8 @@ case "${OPERATOR_COMMAND}" in
 
     # Apply delete
     echo Deleting cr.yaml in ${KUBERNETES_NAMESPACE} namespace
-    pod_name=`kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
-	kubectl --token=${BASE_KUBERNETES_NAMESPACE_SERVICE_ACCOUNT_TOKEN} exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo delete cluster ${CLUSTER_NAME} --no-prompt
+    pod_name=`kubectl_token get pod -l name=pgo-client -o jsonpath="{.items[0].metadata.name}" -n ${KUBERNETES_NAMESPACE}`
+	kubectl_token exec  ${pod_name} -n ${KUBERNETES_NAMESPACE} -- pgo delete cluster ${CLUSTER_NAME} --no-prompt
     sleep 10
 ;;
 'list-backups')
